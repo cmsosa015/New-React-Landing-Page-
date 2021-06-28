c6ddfb758ac7fe35bd802f2b880a6820429a2142
cmsosa015 <cmsosa015@gmail.com>
2021-06-28 20:56:38 +0000

    new version

---
+++ src/js/component/card.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+let Card = {
+    title: 'card title',
+    description: 'description being...',
+    img: 'https://i.pinimg.com/564x/f2/c8/3a/f2c83a8381f5777701aefaff45681af7.jpg'
+}
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,15 +1,16 @@
 import React from "react";
+import Card from "./card.js";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
+		<div className=" card text-center mt-5">
 			<h1>Hello Rigo!</h1>
 			<p>
-				<img src={rigoImage} />
+				<img src={Card.img} />
 			</p>
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working
